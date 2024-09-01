2a7364f1e788715ff48371505f9be58be0e3d528
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-09-01 16:43:52 +0800

    feat(mini-micro-frontend): sub app

---
+++ packages/micro-frontend/test/react-app/src/main.tsx
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>
+// );
+
+export const provider = () => ({
+  render: ({ dom }) => {
+    ReactDOM.createRoot(dom).render(<App />);
+  },
+
+  destroy: ({ dom }) => {
+    ReactDOM.createRoot(dom).unmount();
+  },
+});
